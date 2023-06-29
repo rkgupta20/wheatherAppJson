0f9631ddaa9c4469bc9425c666132a3f9cf6c57d
rkgupta20 <krahul.20kumar@gmail.com>
2023-06-29 23:21:42 +0530

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import datawheather from './file/wheatherData.json';
+import {wheatherData} from './model/wheather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'JsonWheatherApp';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+  
+  public datawheather:wheatherData[] = datawheather;
+
+  searchValue: string | undefined;
+  foundObject: any;
+
+  onSubmit() {
+    this.foundObject = this.datawheather.find(item => item.city === this.searchValue);
+    return false;
+  } 
+  
 }
+
+
---
+++ src/app/model/wheather.model.ts
@@ -0,0 +1,11 @@
+export interface wheatherData {
+    id: number
+    city: string
+    cityId: string
+    humidity: number
+    speed: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  
